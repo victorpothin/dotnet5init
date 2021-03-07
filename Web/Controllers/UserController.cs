ba09b2f859081154fed7fa7b0fb526802d918f10
Victor Pothin <victorpothin1@gmail.com>
2021-03-07 00:00:27 -0300

    Create get by id.

--- Web/Controllers/UserController.cs
+++ Web/Controllers/UserController.cs
@@ -1,5 +1,7 @@
 using Domain.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Domain.Models;
+using Dto.Responses;
 
 namespace Web.Controllers
 {
@@ -7,10 +9,18 @@ namespace Web.Controllers
     [Route("/users")]
     public class UserController : ControllerBase
     {
-        public IUserService UserService { get; }
+        public IUserService _userService { get; }
         public UserController(IUserService userService)
         {
-            this.UserService = userService;
+            this._userService = userService;
+        }
+
+        [HttpGet]
+        public ActionResult<Response<User>> Get([FromQuery] int? id)
+        {
+            if(id == null)
+                return BadRequest("Id is null");
+            return Ok(this._userService.GetById(id.Value));
         }
     }
 }
